0cec30a05e828ae903771ec72834330fd2da0ef2
Vasteman <assaultrifle@list.ru>
2019-02-22 18:42:59 +0400

    Fix count label

--- src/Calculator/calculator.js
+++ src/Calculator/calculator.js
@@ -10,7 +10,8 @@ export default class Calculator extends Component {
 
   state = {
     page: 0,  // 0-5
-    chosenOptions: {} //какие чекбоксы выбраны
+    chosenOptions: {}, //какие чекбоксы выбраны
+    countOfChosenOptions: 0
   }
 
   onMenuBtnClick = (id) => {
@@ -24,26 +25,37 @@ export default class Calculator extends Component {
     obj[id] = !this.state.chosenOptions[id];
     console.log(obj)
     this.setState({
-      chosenOptions: obj
+      chosenOptions: obj,
+      countOfChosenOptions: this.getCountOfChosenOptions(obj)
     })
+}
+
+getCountOfChosenOptions = (obj) => {
+  let count = 0;
+  for (let key in obj) {
+    if (obj[key]) {
+      count++
+    }
   }
+  return count;
+}
 
-  render() {
-    const data = pagesData;
-    const { name } = data[this.state.page];
-    const options = pagesData.map((el) => {
-      return el.options;
-    })
+render() {
+  const data = pagesData;
+  const { name } = data[this.state.page];
+  const options = pagesData.map((el) => {
+    return el.options;
+  })
 
-    return (
-      <div className="calculator clearfix">
-        <Menu labels={data} onMenuBtnClick={this.onMenuBtnClick} />
-        <CalcHeader name={name} count={this.state.chosenOptions.length} />
-        <Options  options={options[this.state.page]} 
-                  chosenOptions={this.state.chosenOptions}
-                  onOptionClick={this.onOptionClick} />
+  return (
+    <div className="calculator clearfix">
+      <Menu labels={data} onMenuBtnClick={this.onMenuBtnClick} />
+      <CalcHeader name={name} count={this.state.countOfChosenOptions} />
+      <Options options={options[this.state.page]}
+        chosenOptions={this.state.chosenOptions}
+        onOptionClick={this.onOptionClick} />
 
-      </div>
-    )
-  }
+    </div>
+  )
+}
 }
